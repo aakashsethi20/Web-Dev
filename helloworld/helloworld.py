87f8ea3d94387e4284ed6c1b78cc34baebf0e26f
Ash Sethi <aakashsethi20@gmail.com>
2016-02-09 14:12:25 -0500

    add google search form

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,9 +1,16 @@
 import webapp2
 
+form="""
+<form action="https://www.google.com/search">
+	<input name="q">
+	<input type="submit">
+</form>
+"""
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, Udacity!')
+        #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
